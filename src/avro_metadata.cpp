a119651273e3f9c7f68bab01315a810ec3541a9e
Tishj <t_b@live.nl>
2026-07-09 11:02:13 +0200

    add missing include

--- src/avro_metadata.cpp
+++ src/avro_metadata.cpp
@@ -2,6 +2,7 @@
 #include "avro_reader.hpp"
 #include "duckdb/common/multi_file/multi_file_reader.hpp"
 #include "duckdb/function/table_function.hpp"
+#include "duckdb/common/file_system.hpp"
 #include "avro_metadata.hpp"
 #include <avro.h>
 
